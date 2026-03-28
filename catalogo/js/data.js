9c155e643155209a95e3952818f81b3d43dfd752
Carl0s-Anderson <carlosanderson30030@gmail.com>
2026-03-27 23:10:22 -0300

    adiciona catalogo e remove os cards de perfil

---
+++ catalogo/js/data.js
@@ -0,0 +1,41 @@
+export const categories = [
+    {
+        title: "Épicos",
+        items: [
+            {
+                img: "https://i0.wp.com/dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2026/01/avatar-fogo-e-cinzas-scaled.jpg?w=960&ssl=1", top10: true, badge: "Clássico", badgeColor: "red", progress: 40,
+                youtube: "https://www.youtube.com/watch?v=yXPWsdT43YE&pp=ygUZYXZhdGFyIDMgdHJhaWxlciBvZmZpY2lhbA%3D%3D"
+            },
+            {
+                img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh90W7cAjNfJZM8Jpf9dnsEzAnVKL3-qKND8S3VHC2MVK3XxgJuei14cECUNB3r3ddE6uVzin90vnVNWhASdCShj9Dsdy_XxFfnEtUiHfwDp0oIQLg8Y0ipF6Kib1maUAZFWYtSPSDj6lXg/s400/wallpaper06-1600x1200.jpg", progress: 80,
+                youtube: "https://www.youtube.com/watch?v=sAetCWdPtUA"
+            },
+            {
+                img: "https://images.wallpapersden.com/image/wxl-avatar-2-the-way-of-water-movie-poster_87742.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=d9MyW72ELq0"
+            },
+            {
+                img: "https://image.tmdb.org/t/p/original/aNK6MA5EApIo0UJE7ZWSYcZBJKy.jpg", progress: 12,
+                youtube: "https://www.youtube.com/watch?v=_inKs4eeHiI"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeaBJfDEAFKZXLZbMj7WO_RcTg82F8zcu5ISoxZb9aO8FfNt7P8kzYwR9TyorgJMqtZyw4GDqRgyblxQICBofjTQf-LhU4OPHps4iKBnU&s=10", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=HhesaQXLuRY" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJrJDQPgSFidIMAyp4GufiLe2UTe-E-VmwjQ&s", top10: true, youtube: "www.youtube.com/watch?v=tjujvMkqWe4&time_continue=1&source_ve_path=NzY3NTg&embeds_referring_euri=https%3A%2F%2Fwww.google.com%2Fsearch%3Fclient%3Dopera-gx%26hs%3DpU0%26sca_esv%3D9b7071d2b58e06a1%26biw%3D1238%26bih%3D604%26sxsrf%3DANbL-n4sHTIBNoIybGwuhUcS6G9" },
+            { img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj0bgT12R3vf9XAavhwvfChv4WGP3Ucx2kO2SUcNXPL5z6K_rN5jbDpfdfyjUOoZ0lwUEGEj7P588Wu3FP1gYGrcSPgUDtst07IUIreTngRtEWdTovnWyCFQ8TlehLhl6XxmRdj9_GlPUg/s512/Imagem+1+-+arte+promocional.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=nHMQBg6Zduc" },
+            { img: "https://img.elo7.com.br/product/685x685/26A83AA/big-poster-serie-better-call-saul-lo01-tamanho-90x60-cm-better-call-saul.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=FAdiEjeNRi0" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://br.web.img2.acsta.net/c_310_420/img/0c/84/0c84950c9b5426343b36883eb0008eb2.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=7CxKksqGrbI" },
+            { img: "https://br.web.img2.acsta.net/img/dd/62/dd620696bab65f7b11ed56b106a7754a.jpg", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=PusopU6FjpA" },
+            { img: "https://cinepop.com.br/wp-content/uploads/2018/12/birdbox_2.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=JeGdtrLQoiY" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThdGZpShjfXXTnzH-UrF5pjiGFKoutZYyWkw&s", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=WbSIdNBO09Q" },
+        ]
+    }
+];
--- script.js
+++ script.js
@@ -16,3 +16,14 @@ themeToggle.addEventListener('click', function() {
   document.body.classList.toggle('light-mode');
   updateButton();
 });
+
+// Armazenar o perfil selecionado no localStorage
+document.querySelectorAll('.perfil a').forEach(link => {
+  link.addEventListener('click', function(event) {
+    const nome = this.querySelector('.nome').textContent;
+    const imagemSrc = this.querySelector('img').src;
+    
+    localStorage.setItem('perfilAtivoNome', nome);
+    localStorage.setItem('perfilAtivoImagem', imagemSrc);
+  });
+});
